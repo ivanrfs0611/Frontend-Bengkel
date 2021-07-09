c98df4441f3031701e5e6d2b75aecc5c3eb9e487
Ivan Rafi Susanto <susantoivan0611@gmail.com>
2021-07-09 18:35:09 +0700

    little touch on homepage

--- src/views/pages/Homepage/Homepage.js
+++ src/views/pages/Homepage/Homepage.js
@@ -1,51 +1,64 @@
-import React from "react"
+import React from "react";
 import {
   CDataTable,
   CButton,
   CCard,
   CCardBody,
   CCol,
-  CRow
-} from '@coreui/react'
-import { DocsLink } from 'src/reusable'
-import usersData from '../../users/UsersData'
-import CIcon from '@coreui/icons-react'
+  CRow,
+} from "@coreui/react";
+import { DocsLink } from "src/reusable";
+import usersData from "../../users/UsersData";
+import CIcon from "@coreui/icons-react";
 
-
-const fields = ['ID','NAMA_LENGKAP', 'NO_TELFON', 'EMAIL', 'ALAMAT', 'TANGGAL_MASUK']
+const fields = [
+  "ID",
+  "NAMA_LENGKAP",
+  "NO_TELFON",
+  "EMAIL",
+  "ALAMAT",
+  "TANGGAL_MASUK",
+];
 
 const Homepage = () => {
-
   return (
-<><br></br><br></br><br></br><br></br><br></br><br></br>
-  <div class='table'>
-    <div>
-      <CCol className='header'>
-        <CButton className='button'>
-          <CIcon name="cil-user-follow" className='icon' />
-            <strong><span className="mfs-2">ADD EMPLOYEE</span></strong>
-          </CButton>
-        <h2 class='text'>Employees</h2>
-      </CCol>
-    </div>
-      <CRow>
-        <CCol>
-          <CCard className='card'>
-            <CCardBody className='datatable'>
-              <CDataTable 
-              items={usersData}
-              fields={fields}
-              itemsPerPage={10}
-              pagination
-              striped
-              />
-            </CCardBody>
-          </CCard>
-        </CCol>
-      </CRow>
-    </div>
-  </>
-  )
-}
+    <>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div class="table">
+        <div>
+          <CCol className="header">
+            <CButton className="button">
+              <CIcon name="cil-user-follow" className="icon" />
+              <strong>
+                <span className="mfs-2">ADD EMPLOYEE</span>
+              </strong>
+            </CButton>
+            <h2 class="text">Employees</h2>
+          </CCol>
+        </div>
+        <CRow>
+          <CCol>
+            <CCard className="card">
+              <CCardBody className="datatable">
+                <CDataTable
+                  items={usersData}
+                  fields={fields}
+                  itemsPerPage={10}
+                  pagination
+                  striped
+                />
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </div>
+    </>
+  );
+};
 
-export default Homepage
+export default Homepage;
